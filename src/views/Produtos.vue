7d6429a1a985712a028d24499d748e6cda9a9bbc
JulliaW <weberjullia44@gmail.com>
2025-08-05 19:39:49 -0300

    FEAT: Adicionando transição no modal

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -12,28 +12,42 @@
 
     <Listagem ref="listagemRef" />
 
-    <div
-      v-if="showModal"
-      class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-white/60 dark:bg-gray-800/60"
-    >
+    <Transition name="fade">
       <div
-        class="bg-white dark:bg-gray-800 rounded-lg shadow-2xl p-6 w-full max-w-xl relative ring-1 ring-black/10 dark:ring-white/10"
-      >
-        <button
-          @click="showModal = false"
-          class="absolute top-2 right-2 text-gray-500 hover:text-red-500 text-xl"
-        >
-          <i class="fa-solid fa-xmark"></i>
-        </button>
+        v-if="showModal"
+        class="fixed inset-0 z-40 flex items-center justify-center backdrop-blur-sm bg-white/60 dark:bg-gray-800/60"
+        @click="fecharModalEAtualizar"
+      />
+    </Transition>
 
-        <Cadastro @salvou="fecharModalEAtualizar" />
+    <Transition
+      enter-active-class="transition ease-out duration-300"
+      enter-from-class="opacity-0 scale-95"
+      enter-to-class="opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-200"
+      leave-from-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-95"
+    >
+      <div v-if="showModal" class="fixed inset-0 z-50 flex items-center justify-center">
+        <div
+          class="bg-white dark:bg-gray-800 rounded-lg shadow-2xl p-6 w-full max-w-xl relative ring-1 ring-black/10 dark:ring-white/10"
+        >
+          <button
+            @click="fecharModalEAtualizar"
+            class="absolute top-2 right-2 text-gray-500 hover:text-red-500 text-xl"
+          >
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+
+          <Cadastro @close="fecharModalEAtualizar" />
+        </div>
       </div>
-    </div>
+    </Transition>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, Transition } from 'vue'
 import Listagem from './Listagem.vue'
 import Cadastro from './Cadastro.vue'
 
